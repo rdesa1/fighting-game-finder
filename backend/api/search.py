29b52e1ec06c43f3e9dcde0b323c0f86314b0e94
Richard De Sa Jr. <rdesa1@umassd.edu>
2025-07-28 14:27:57 -0400

    API can now successfuly query the database and return JSON

--- backend/api/search.py
+++ backend/api/search.py
@@ -1,8 +1,48 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+import os
+from dotenv import load_dotenv # for manipulating environment variables
+import psycopg # postgreSQL module
 
 # Create a blueprint instance
 search_bp = Blueprint('search', __name__)
 
-@search_bp.route('/<query>', methods=['GET'])
-def get_query_results(query):
-    return query
+# Query the database for locations that closely match the user's input
+@search_bp.route('/<state>', methods=['GET'])
+@search_bp.route('/<state>/<city>')
+def get_query_results(state, city=None):
+
+    # load environment variables from a .env file into the application's environment
+    load_dotenv()
+
+    # obtain the environment variables for establishing a connection
+    DATABASE_USER = os.getenv('DATABASE_USER')
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+    DATABASE_HOST_NAME = os.getenv('DATABASE_HOST_NAME')
+    DATABASE_PORT = os.getenv('DATABASE_PORT')
+    DATABASE_NAME = os.getenv('DATABASE_NAME')
+
+    # Connect to an existing database
+    try:
+        with psycopg.connect(f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST_NAME}:{DATABASE_PORT}/{DATABASE_NAME}") as conn:
+            with conn.cursor() as cur:
+
+                if city:
+
+                    # Normalize the input
+                    city = city.capitalize() 
+
+                    # Query the database for active locations from the same state ("Subnational" in the table)
+                    postgreSQL_select_Query = 'SELECT * from "Locals" WHERE "Country" = %s AND "Subnational" = %s AND "Metro Area" = %s AND "Status" = %s'
+                    cur.execute(postgreSQL_select_Query, ("USA", state, city, "Active"))
+                    results = cur.fetchall()
+
+                else:
+                    # Query the database for active locations from the same state ("Subnational" in the table)
+                    postgreSQL_select_Query = 'SELECT * from "Locals" WHERE "Country" = %s AND "Subnational" = %s AND "Status" = %s'
+                    cur.execute(postgreSQL_select_Query, ("USA", state, "Active"))
+                    results = cur.fetchall()
+
+    except (Exception, psycopg.Error) as error:
+        print ("Error fetching data from PostgreSQL table", error)
+
+    return jsonify(results)
--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,6 @@
-from flask import Flask, request, jsonify # import flask module
-import pandas as pd # pandas for creating a dataframe from the spreadsheet
-import psycopg # postgreSQL module
-import os # for obtaining environment variables
-from dotenv import load_dotenv # for manipulating environment variables
-from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask # import flask module
+from flask_debugtoolbar import DebugToolbarExtension # For the Flask debug toolbar
+import os
 from api.search import search_bp  # Import blueprint
   
 # create the app
@@ -14,49 +11,6 @@ app.register_blueprint(search_bp, url_prefix='/search')
 
 # set a 'SECRET_KEY' to enable the Flask session cookies
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
-# load environment variables from a .env file into the application's environment
-load_dotenv()
-
-# obtain the environment variables for establishing a connection
-DATABASE_USER = os.getenv('DATABASE_USER')
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
-DATABASE_HOST_NAME = os.getenv('DATABASE_HOST_NAME')
-DATABASE_PORT = os.getenv('DATABASE_PORT')
-DATABASE_NAME = os.getenv('DATABASE_NAME')
-
-# Connect to an existing database
-with psycopg.connect(f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST_NAME}:{DATABASE_PORT}/{DATABASE_NAME}") as conn:
-
-    # Open a cursor to perform database operations
-    with conn.cursor() as cur:
-
-        # Execute a command: this creates a new table, if it doesn't already exist
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS test (
-                id serial PRIMARY KEY,
-                num integer,
-                data text)
-            """)            
-
-        # Pass data to fill a query placeholders and let Psycopg perform
-        # the correct conversion (no SQL injections!)
-        '''cur.execute(
-            "INSERT INTO test (num, data) VALUES (%s, %s)",
-            (100, "abc'def"))'''
-
-        # Query the database and obtain data as Python objects.
-        cur.execute("SELECT * FROM test")
-        cur.fetchone()
-        # will return (1, 100, "abc'def")
-
-        # You can use `cur.fetchmany()`, `cur.fetchall()` to return a list
-        # of several records, or even iterate on the cursor
-        for record in cur:
-            print(record)
-
-        # Make the changes to the database persistent
-            conn.commit()
 
 # a simple page that says hello
 @app.route('/')
