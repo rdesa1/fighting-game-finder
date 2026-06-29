f2d15a7d4b12adbfc90676a0ddd3dda4df0b5fb7
Richard De Sa Jr. <rdesa1@umassd.edu>
2026-06-29 15:58:25 -0400

    Made divs for each field of the result data.

--- frontend/src/components/local.tsx
+++ frontend/src/components/local.tsx
@@ -6,24 +6,37 @@ interface LocalProps {
 
 export default function Local({ local }: LocalProps) {
      return (
-          <div id= "local">
-               <li>
-                    <h3>{local.name}</h3>
+          <li className="local-card">
+               <h3>{local.name}</h3>
 
+               <div className="local-section">
                     <p>
-                         <b>Venue:</b> {local.venue}
+                         <strong>Venue: </strong>
+                         {local.venue}
                     </p>
+               </div>
 
+               <div className="local-section">
                     <p>
-                         <b>Address:</b> {local.metro_area}, {local.subnational}, {local.address}
+                         <strong>Address: </strong>
+                         {local.metro_area}, {local.subnational}, {local.address}
                     </p>
+               </div>
 
+               <div className="local-section">
                     <p>
-                         <b>Frequency:</b> {local.frequency} on {local.day}
+                         <strong>Schedule: </strong>
+                         {local.frequency} on {local.day}
                     </p>
+               </div>
 
-                    <p> <b>Type:</b> {local.event_type}</p>
-               </li>
-          </div>
+               <div className="local-section">
+                    <p>
+                         <strong>Type: </strong>
+                         {local.event_type}
+                    </p>
+               </div>
+
+          </li>
      );
 }
