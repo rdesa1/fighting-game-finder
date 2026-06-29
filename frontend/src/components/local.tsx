4efd89c6bec65f5d627e7ea323b72dd7ffcf3a03
Richard De Sa Jr. <rdesa1@umassd.edu>
2026-06-29 16:59:30 -0400

    Implemented a basic CSS layout for the search results.

--- frontend/src/components/local.tsx
+++ frontend/src/components/local.tsx
@@ -12,23 +12,24 @@ export default function Local({ local }: LocalProps) {
 
                <div className="local-section">
                     <strong>Venue: </strong>
-                    {local.venue}
+                    <span>{local.venue}</span>
+
                </div>
 
                <div className="local-section">
                     <strong>Address: </strong>
-                    {local.metro_area}, {local.subnational}, {local.address}
+                    <span>{local.metro_area}, {local.subnational}, {local.address}</span>
                </div>
 
                <div className="local-section">
                     <strong>Schedule: </strong>
-                    {local.frequency} on {local.day}
+                    <span>{local.frequency} on {local.day}</span>
 
                </div>
 
                <div className="local-section">
                     <strong>Type: </strong>
-                    {local.event_type}
+                    <span>{local.event_type}</span>
                </div>
 
           </li>
