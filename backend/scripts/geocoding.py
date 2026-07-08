2de30566c144275fdefcd15193ac37bcc75f164f
Richard De Sa Jr. <rdesa1@umassd.edu>
2026-07-08 15:39:09 -0400

    Wrote a script that successfully geocodes the address of a database entry and caches it within the database.

--- backend/scripts/geocoding.py
+++ backend/scripts/geocoding.py
@@ -1,5 +1,9 @@
+# This script is for obtaining and caching longitude and latidude data points 
+# within the database, using the nominatum geocoding API.
+
 import os
 import psycopg
+import requests
 
 from dotenv import load_dotenv
 from psycopg.rows import dict_row
@@ -12,14 +16,13 @@ DATABASE_HOST_NAME = os.getenv("DATABASE_HOST_NAME")
 DATABASE_PORT = os.getenv("DATABASE_PORT")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 
-with psycopg.connect(
+DATABASE_URL = (
     f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}"
-    f"@{DATABASE_HOST_NAME}:{DATABASE_PORT}/{DATABASE_NAME}",
-    row_factory=dict_row
-) as conn:
+    f"@{DATABASE_HOST_NAME}:{DATABASE_PORT}/{DATABASE_NAME}"
+)
 
+with psycopg.connect(DATABASE_URL, row_factory=dict_row) as conn:
     with conn.cursor() as cur:
-
         query = '''
         SELECT
             id,
@@ -28,13 +31,74 @@ with psycopg.connect(
             "Metro Area" AS metro_area,
             "Address" AS address
         FROM "Locals"
-        WHERE latitude IS NULL
-        OR longitude IS NULL
+        WHERE "Country" = 'USA'
+        AND "Address" IS NOT NULL
+        AND (latitude IS NULL OR longitude IS NULL)
         ORDER BY id;
         '''
 
         cur.execute(query)
         locals_to_geocode = cur.fetchall()
 
-for local in locals_to_geocode:
-    print(local)
+
+if not locals_to_geocode:
+    print("No rows need geocoding.")
+    exit()
+
+local = locals_to_geocode[0]
+
+search_query = (
+    f'{local["address"]}, '
+    f'{local["subnational"]}, USA'
+)
+
+params = {
+    "q": search_query,
+    "format": "jsonv2",
+    "limit": 1
+}
+
+headers = {
+    "User-Agent": "FightingGameFinder/1.0"
+}
+
+response = requests.get(
+    "https://nominatim.openstreetmap.org/search",
+    params=params,
+    headers=headers,
+    timeout=10
+)
+
+response.raise_for_status()
+
+data = response.json()
+
+print(search_query)
+print(data)
+
+if data:
+    latitude = float(data[0]["lat"])
+    longitude = float(data[0]["lon"])
+
+    print("Latitude:", latitude)
+    print("Longitude:", longitude)
+else:
+    print("No geocoding result found.")
+    exit()
+
+with psycopg.connect(DATABASE_URL) as conn:
+    with conn.cursor() as cur:
+        cur.execute(
+            '''
+            UPDATE "Locals"
+            SET latitude = %s,
+                longitude = %s
+            WHERE id = %s;
+            ''',
+            (latitude, longitude, local["id"])
+
+    )
+
+    conn.commit()
+
+print(f'Updated row {local["id"]}')
