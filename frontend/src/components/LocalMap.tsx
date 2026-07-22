b694ce805cdea7c199436e674c868dad51c0878d
Richard De Sa Jr. <rdesa1@umassd.edu>
2026-07-22 13:58:36 -0400

    Map automatically zooms into the state that's been queried.

--- frontend/src/components/LocalMap.tsx
+++ frontend/src/components/LocalMap.tsx
@@ -3,6 +3,9 @@
 import "leaflet/dist/leaflet.css"
 import type { Local as LocalType } from "../types/local.ts";
 import "../styles/LocalMap.css" // Without an explicit height, the map will not render
+import { useEffect } from "react";
+import { useMap } from "react-leaflet";
+import L from "leaflet";
 
 import {
      MapContainer,
@@ -13,6 +16,32 @@ import {
 
 interface LocalMapProps {
      locals: LocalType[];
+}
+
+// helper function to zoom into the state that has been searched
+function FitBounds({ locals }: LocalMapProps) {
+     const map = useMap();
+
+     useEffect(() => {
+          if (locals.length === 0) {
+               return;
+          }
+
+          // compute the smallest rectangle containing all the marker coordinates
+          const bounds = L.latLngBounds(
+               locals.map((local) => [
+                    local.latitude as number,
+                    local.longitude as number
+               ])
+          );
+
+          // zooms and pans over to the rectangle containing all marker coordinates
+          map.fitBounds(bounds, {
+               padding: [50, 50]
+          });
+     }, [locals, map]);
+
+     return null;
 }
 
 export default function LocalMap({ locals }: LocalMapProps) {
@@ -36,6 +65,7 @@ export default function LocalMap({ locals }: LocalMapProps) {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
                />
 
+               <FitBounds locals={localsWithCoordinates } />
                
                {localsWithCoordinates.map((local) => ( // render one marker per latitude, longitude coordinate pair
                     <Marker
