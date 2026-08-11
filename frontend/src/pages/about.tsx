ddb6043cf8cc4cccbc45153197961ca672e13ea9
Richard De Sa Jr. <rdesa1@umassd.edu>
2026-08-11 14:58:25 -0400

    Implemented a functional "back" link that takes the user back to the home page.

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router";
+import { ArrowLeft } from "lucide-react";
+import "../styles/about.css";
+
 export default function About() {
+
+
      return (
-          <main>
+          <main className="about-page">
+
+               <Link
+                    to="/"
+                    className="back-link"
+               >
+
+                    <ArrowLeft size={18} />
+                    <span>Back</span>
+               </Link>
+
                <h1>About Fighting Game Finder</h1>
 
                <p>
