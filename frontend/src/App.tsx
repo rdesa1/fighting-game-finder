9387919986398a2cc5efd7fa2b8eb029769716f6
Richard De Sa Jr. <rdesa1@umassd.edu>
2026-09-01 17:12:36 -0400

    Deleted the unused route path to "search_results" from App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,7 +19,6 @@ function App() {
 
                <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/search_results" element={<Search_results />} />
                     <Route path="/test" element={<Test />} />
                     <Route path="/about" element={<About />} />
                </Routes>
